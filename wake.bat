96a9c46433ab6a0dc50ce0e7d21b85c61f6aac30
Qiuchi0918 <64412747+Qiuchi0918@users.noreply.github.com>
2020-05-16 15:52:33 +0800

    改写死ip为获取ip

--- wake.bat
+++ wake.bat
@@ -1,8 +1,11 @@
 setlocal enabledelayedexpansion
 for %%j in (*.zim) do set k=!k! %%j
 setlocal disabledelayedexpansion
+for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
+	set IP=%%a
+)
 
-start http://192.168.0.104:8080/
+start http://%IP%:8080/
 for %%i in (chrome,firefox,msedge) do (
 	for /f "tokens=2" %%a in ('tasklist /FI "imagename EQ %%i.exe" /NH') do (
 	kiwix-serve.exe -a %%a -d -p 8080 %k% && goto END
