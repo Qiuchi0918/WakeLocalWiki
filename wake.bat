34370eb6f362cc22f02f4427ef52700af6656429
Qiuchi0918 <64412747+Qiuchi0918@users.noreply.github.com>
2020-05-16 16:29:12 +0800

    删错了

--- wake.bat
+++ wake.bat
@@ -8,7 +8,7 @@ for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
 start http://%IP%:8080/
 for %%i in (chrome,firefox,msedge) do (
 	for /f "tokens=2" %%a in ('tasklist /FI "imagename EQ %%i.exe" /NH') do (
-	kiwix-serve.exe -a %%a -d -p 8080 %k% && goto END
+	echo %%a | findstr "[0-9]" && kiwix-serve.exe -a %%a -d -p 8080 %k% && goto END
 )
 )
 :END
