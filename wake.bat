de9c290b2f640855d4e17c5ffacd180b52706990
Qiuchi0918 <64412747+Qiuchi0918@users.noreply.github.com>
2020-05-16 15:41:56 +0800

    emmm

--- wake.bat
+++ wake.bat
@@ -5,7 +5,7 @@ setlocal disabledelayedexpansion
 start http://192.168.0.104:8080/
 for %%i in (chrome,firefox,msedge) do (
 	for /f "tokens=2" %%a in ('tasklist /FI "imagename EQ %%i.exe" /NH') do (
-	echo %%a | findstr "[0-9]" && kiwix-serve.exe -a %%a -d -p 8080 %k% && goto END
+	kiwix-serve.exe -a %%a -d -p 8080 %k% && goto END
 )
 )
 :END
